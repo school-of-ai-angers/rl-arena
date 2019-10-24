d9675ac9c325c8223a6ee86746d6897fc8f080f0
Guilherme <sitegui@sitegui.com.br>
2019-10-24 23:03:14 +0200

    Fix run_duel to mount host media folder

--- run_duel.sh
+++ run_duel.sh
@@ -1,21 +1,24 @@
 #!/bin/bash -e
 
-# This file will run duel to completion
-# It receives its parameters via environment variables:
+if [[ $# -lt 4 ]]; then
+    echo "Run a duel to completion"
+    echo "Usage: $0 <namespace> <image_1> <image_2> <environment> <host_cwd> <output_file>"
+    exit 1
+fi
 
-# Select a new unique namespace
-NAMESPACE=$(openssl rand -hex 8)
+NAMESPACE="$1"
+
+# Make sure all previous containers are down
+docker-compose --project-name "$NAMESPACE" --file run_duel/docker-compose.yml down
 
 # Start duel runner
 DUEL_NAMESPACE="$NAMESPACE" \
-DUEL_IMAGE_1="$DUEL_IMAGE_1" \
-DUEL_IMAGE_2="$DUEL_IMAGE_2" \
-DUEL_ENVIRONMENT="$DUEL_ENVIRONMENT" \
-DUEL_OUTPUT_FILE="$DUEL_OUTPUT_FILE" \
+DUEL_IMAGE_1="$2" \
+DUEL_IMAGE_2="$3" \
+DUEL_ENVIRONMENT="$4" \
+HOST_CWD="$5" \
+DUEL_OUTPUT_FILE="$6" \
     docker-compose --project-name "$NAMESPACE" --file run_duel/docker-compose.yml \
-    up run_duel || true
+    up run_duel
 
-docker-compose --project-name "$NAMESPACE" --file run_duel/docker-compose.yml logs
-
-# Turn make sure all containers are down
 docker-compose --project-name "$NAMESPACE" --file run_duel/docker-compose.yml down
--- run_duel/main.py
+++ run_duel/main.py
@@ -1,9 +1,12 @@
 from yaml import safe_load
-import os
 from run_duel.remote_player import RemotePlayer
 from importlib import import_module
 from run_duel.run_match import run_match
 import json
+import logging
+import os
+logging.basicConfig(level=os.environ['LOG_LEVEL'])
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     # Read args
@@ -13,25 +16,25 @@ if __name__ == "__main__":
     output_file = os.environ['DUEL_OUTPUT_FILE']
     namespace = os.environ['DUEL_NAMESPACE']
 
-    print('=== Load environment details ===')
+    logger.info('Load environment details')
     with open(f'environments/{environment}/info.yml') as fp:
         environment_info = safe_load(fp)
     cpu_limit = environment_info['cpu_limit']
     memory_limit = environment_info['memory_limit']
     num_matches = environment_info['num_matches_in_duel']
 
-    print('=== Create environment ===')
+    logger.info('Create environment')
     env = import_module(
         f'environments.{environment}.environment').Environment()
 
-    print('=== Boot player 1 ===')
+    logger.info('Boot player 1')
     player_1 = RemotePlayer(
         image_1, namespace, cpu_limit, memory_limit, 'player_1')
-    print('=== Boot player 2 ===')
+    logger.info('Boot player 2')
     player_2 = RemotePlayer(
         image_2, namespace, cpu_limit, memory_limit, 'player_2')
 
-    result = {
+    duel_result = {
         'num_matches': 0,
         'player_1_errors': 0,
         'player_2_errors': 0,
@@ -45,19 +48,30 @@ if __name__ == "__main__":
     }
 
     # Play matches (alternating who starts)
+    match_num = 0
+
     def handle_match(player_1_starts):
+        global match_num
         match = run_match(env, player_1, player_2, player_1_starts)
-        result['num_matches'] += 1
-        result[match['result'].lower() + 's'] += 1
-        result['player_1_score'] += match['player_1_score']
-        result['player_2_score'] += match['player_2_score']
-        result['matches'].append(match)
-    for i in range(0, num_matches, 2):
-        print(f'Play match {i}')
+        if 'ERROR' in match['result']:
+            logger.error(
+                f'Match {match_num}: {match["result"]} with {match["error_msg"]}')
+        else:
+            logger.info(f'Match {match_num}: {match["result"]}')
+
+        # Update duel
+        match_num += 1
+        duel_result['num_matches'] += 1
+        duel_result[match['result'].lower() + 's'] += 1
+        duel_result['player_1_score'] += match['player_1_score']
+        duel_result['player_2_score'] += match['player_2_score']
+        duel_result['matches'].append(match)
+
+    for _ in range(0, num_matches, 2):
         handle_match(True)
         handle_match(False)
 
-    print('=== Write to file ===')
+    logger.info(f'Write to {output_file}')
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
     with open(output_file, 'w') as fp:
-        json.dump(result, fp)
+        json.dump(duel_result, fp)
