182a49ab8e8aa11fc86426dd2f40f3573423ac95
Guilherme <sitegui@sitegui.com.br>
2019-10-27 17:46:17 +0100

    Install certificate on nginx boot

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -e
 
+# Prepare nginx config
 ALLOWED_HOSTS_SPACED=${ALLOWED_HOSTS/,/ }
-
 sed -i -e "s/ALLOWED_HOSTS_SPACED/$ALLOWED_HOSTS_SPACED/" /etc/nginx/conf.d/default.conf
+
+# Install certificate
+if [[ -d "/etc/letsencrypt/live/$ALLOWED_HOSTS" ]]; then
+    echo "Install certificates"
+    certbot install --nginx --cert-name "$ALLOWED_HOSTS" --redirect
+fi
 
 exec nginx -g 'daemon off;'
